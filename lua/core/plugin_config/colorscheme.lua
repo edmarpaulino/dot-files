3a4d404cb64dc3f8a1cd683f5791d1ad9ce76bf6
Edmar Paulino <edmarpaulino9@gmail.com>
2023-04-07 19:30:26 -0300

    add transparency

--- lua/core/plugin_config/colorscheme.lua
+++ lua/core/plugin_config/colorscheme.lua
@@ -1,2 +1,8 @@
 vim.o.termguicolors = true
 vim.cmd [[ colorscheme catppuccin ]]
+-- Transparency
+vim.api.nvim_command('hi Normal ctermbg=none guibg=none')
+vim.api.nvim_command('hi NormalSB ctermbg=none guibg=none')
+vim.api.nvim_command('hi NormalNC ctermbg=none guibg=none')
+vim.api.nvim_command('hi NvimTreeNormal ctermbg=none guibg=none')
+vim.api.nvim_command('hi NvimTreeNormalNC ctermbg=none guibg=none')
